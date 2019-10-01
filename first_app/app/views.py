f85519a139c99b93673274c6f74c8694a7de6d1a
Saurabh Goel <Saurabh.Goel@rsystems.com>
2019-10-01 18:54:50 +0530

    'initial-commit'

---
+++ first_app/app/views.py
@@ -0,0 +1,149 @@
+from flask import render_template, flash
+from flask_appbuilder.models.sqla.interface import SQLAInterface
+from flask_appbuilder import ModelView, ModelRestApi
+
+from . import appbuilder, db
+
+#------for all models
+from .models import *
+#------for all forms
+from .forms import *
+
+
+
+from flask_appbuilder import AppBuilder, BaseView, expose, has_access
+
+from flask_appbuilder import SimpleFormView
+from flask_babel import lazy_gettext as _
+
+
+class MyView(BaseView):
+
+    default_view = 'method1'
+
+    @expose('/method1/')
+    @has_access
+    def method1(self):
+        # do something with param1
+        # and return to previous page or index
+        return self.render_template('method3.html',
+                            param1 = 'Hello')
+
+    @expose('/method2/<string:param1>')
+    @has_access
+    def method2(self, param1):
+        # do something with param1
+        # and render template with param
+        param1 = 'Goodbye %s' % (param1)
+        return self.render_template('method3.html',
+                            param1 = param1)
+    
+    @expose('/method3/<string:param1>')
+    @has_access
+    def method3(self, param1):
+        # do something with param1
+        # and render template with param
+        param1 = 'Goodbye %s' % (param1)
+        self.update_redirect()
+        return self.render_template('method3.html',
+                            param1 = param1)
+
+class MyFormView(SimpleFormView):
+    form = MyForm
+    form_title = 'This is my first form view'
+    message = 'My form submitted'
+
+    def form_get(self, form):
+        form.field1.data = 'This was prefilled'
+
+    def form_post(self, form):
+        # post process form
+        flash(self.message, 'info')
+
+class ContactModelView(ModelView):
+    datamodel = SQLAInterface(Contact)
+
+    label_columns = {'contact_group':'Contacts Group'}
+    list_columns = ['name','personal_cellphone','birthday','contact_group']
+
+    show_fieldsets = [
+        (
+            'Summary',
+            {'fields': ['name', 'address', 'contact_group']}
+        ),
+        (
+            'Personal Info',
+            {'fields': ['birthday', 'personal_phone', 'personal_cellphone'], 'expanded': False}
+        ),
+    ]
+
+class GroupModelView(ModelView):
+    datamodel = SQLAInterface(ContactGroup)
+    related_views = [ContactModelView]
+
+
+
+appbuilder.add_view(MyView, "Method1", category='My View')
+appbuilder.add_link("Method2", href='/myview/method2/john', category='My View')
+appbuilder.add_link("Method3", href='/myview/method3/john', category='My View')
+appbuilder.add_view(MyFormView, "My form View", icon="fa-group", label=_('My form View'),
+                     category="My Forms", category_icon="fa-cogs")
+
+
+"""
+    Create your Model based REST API::
+
+    class MyModelApi(ModelRestApi):
+        datamodel = SQLAInterface(MyModel)
+
+    appbuilder.add_api(MyModelApi)
+
+
+    Create your Views::
+
+
+    class MyModelView(ModelView):
+        datamodel = SQLAInterface(MyModel)
+
+
+    Next, register your Views::
+
+
+    appbuilder.add_view(
+        MyModelView,
+        "My View",
+        icon="fa-folder-open-o",
+        category="My Category",
+        category_icon='fa-envelope'
+    )
+"""
+
+"""
+    Application wide 404 error handler
+"""
+
+
+@appbuilder.app.errorhandler(404)
+def page_not_found(e):
+    return (
+        render_template(
+            "404.html", base_template=appbuilder.base_template, appbuilder=appbuilder
+        ),
+        404,
+    )
+
+
+db.create_all()
+appbuilder.add_view(
+    GroupModelView,
+    "List Groups",
+    icon = "fa-folder-open-o",
+    category = "Contacts",
+    category_icon = "fa-envelope"
+)
+appbuilder.add_view(
+    ContactModelView,
+    "List Contacts",
+    icon = "fa-envelope",
+    category = "Contacts"
+)
